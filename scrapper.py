3a4554929733276a19daa0873282ae86137133d9
Bishal Sarangkoti <sarangbishal@gmail.com>
2019-02-26 00:29:50 +0545

    fix linux-windows compatibility issues

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 import pdfkit
 import os
+import platform
 import requests
 import re
 
@@ -29,6 +30,10 @@ print("                                                |_|   |_|                
 print("                                                            Author: Bishal Sarangkoti(sarangbishal.github.io)")
 
 print("\nThe starting and ending contest number separated by space")
+
+def isLinux():
+    return platform.system() == "Linux"
+
 start, end = 1, 1
 try:
     start, end = map(int, input().split())
@@ -46,20 +51,22 @@ DIR = "Downloaded PDFs/"
 
 #Regex Pattern for contest name
 header_pattern =  re.compile(r'<div style="text-align: center; font-size: 1\.8rem; margin-bottom: 0\.5em;"\s*class="caption">(.+)</div>')
+
 invalid_character = re.compile(r'&|;|-|/|$|<|>|\*|\?|\||"|:|\\')
 for contest_num in range(start, end + 1):
     get_url = BASE_URL + str(contest_num) + "/problems"
 
-    #Path to wkhtmltopdf
-    path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-    config = pdfkit.configuration(wkhtmltopdf = path_wkthmltopdf)
+   
 
     html = requests.get(BASE_URL + str(contest_num) + "/problems")
+    print("\n\n")
     print(get_url)
     not_found_pattern = r'< div class ="message" > No such contest < / div >'
     if re.match(not_found_pattern, html.text):
         continue
     contest_title = "".join(re.findall(header_pattern, html.text))
+
+    #Remove invalid characters
     contest_title = re.sub(invalid_character,"", contest_title)
     print("Converting contest {}".format(contest_title))
     file_name = DIR + contest_title + ".pdf"
@@ -67,4 +74,10 @@ for contest_num in range(start, end + 1):
     if os.path.exists(file_name):
         print("Contest {} already exists".format(contest_title))
         continue
-    pdfkit.from_url(get_url, file_name, configuration = config)
+    if isLinux():
+        pdfkit.from_url(get_url, file_name)
+    else:
+        #Path to wkhtmltopdf
+        path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+        config = pdfkit.configuration(wkhtmltopdf = path_wkthmltopdf)
+        pdfkit.from_url(get_url, file_name, configuration = config)
